052e159426727925062f4bc708c5d22bea5d1579
rohitdighe19 <rohitdighe47@gmail.com>
2021-06-22 15:47:04 +0530

    intial commit

---
+++ EmployeeWage.java
@@ -0,0 +1,12 @@
+
+public class EmployeeWage {
+	public static void main(String[] args) {
+		int isFullTime = 1;
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		if (empCheck == isFullTime)
+			System.out.println("Employee is present");
+		else
+			System.out.println("Employee is abscent");
+	}
+
+}
